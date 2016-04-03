0ed940a491a9154193661f4d092b4ee4c506bc60
bastienricoeur <bastienricoeur@gmail.com>
2016-04-03 19:28:04 +0200

    ImpCode
    push

---
+++ src/graph/graphImplementation.java
@@ -0,0 +1,100 @@
+package graph;
+import java.util.*;
+
+public class graphImplementation implements Graph {
+
+	ArrayList<Vertex> vertexList = new ArrayList<Vertex>();
+	ArrayList<Edge> edgeList = new ArrayList<Edge>();
+	
+	
+	@Override
+	public Edge addEdge(Vertex arg0, Vertex arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean addEdgeToGraph(Vertex arg0, Vertex arg1, Edge arg2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addVertexToGraph(Vertex arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsEdge(Edge arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsVertex(Vertex arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Edge[] getAllEdges() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Vertex[] getAllVertex() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Edge getEdge(Vertex arg0, Vertex arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAdjacent(Edge arg0, Edge arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isDirected(Edge arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Edge[] isInEdges(Vertex arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean removeAllEdges(Edge[] arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeAllVertices(Vertex[] arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeEdge(Edge arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeVertex(Vertex arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
